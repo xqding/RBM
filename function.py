8536876beddb94a81b8f99c9caf69b76a5b23896
Xinqiang Ding <xqding@umich.edu>
2018-11-09 15:30:59 -0500

    add function to calculate model expection using MBAR

--- function.py
+++ function.py
@@ -5,8 +5,11 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+import math
 import itertools
 import sys    
+sys.path.append("/home/xqding/course/projectsOnGitHub/FastMBAR/FastMBAR")
+from FastMBAR import *
 
 def calculate_log_partition_function(W, b_v, b_h):
     num_hidden_units = len(b_h)
@@ -29,7 +32,7 @@ def calculate_log_partition_function(W, b_v, b_h):
     logZ = torch.log(torch.sum(torch.exp(logZ_h))) + tmp    
     return logZ
 
-def calculate_model_expection(W, b_v, b_h):
+def calculate_model_expectation_brute_force(W, b_v, b_h):
     num_visible_units = len(b_v)    
     num_hidden_units = len(b_h)
 
@@ -88,3 +91,91 @@ def calculate_model_expection(W, b_v, b_h):
             # assert(diff <= 1e-4)
     
     return prob
+
+def calculate_model_expectation_mbar(W, b_v, b_h, samples_v, samples_h):
+    '''
+    calcualte the model expection of <v_i*h_j> using MBAR approach
+    '''
+
+    ## make sure dimensions of parameters agree with each other
+    num_visible_units = len(b_v)
+    num_hidden_units = len(b_h)
+    assert(num_visible_units == W.shape[0])
+    assert(num_hidden_units == W.shape[1])
+    
+    num_samples = samples_v.shape[0]
+    assert(num_samples == samples_h.shape[0])
+    assert(samples_v.shape[1] == num_visible_units)
+    assert(samples_h.shape[1] == num_hidden_units)
+
+    ## calculate the model expection of <v_i*h_j> for each pair of (i,j)
+    ## using MBAR
+    prob_mbar = torch.zeros((num_visible_units, num_hidden_units))
+    
+    for i in range(num_visible_units):
+        for j in range(num_hidden_units):
+            tmp_samples_v = samples_v.clone().float()
+            tmp_samples_h = samples_h.clone().float()
+
+            ## for each sample, calculate its energy for four different states:
+            ## 1. set v_i = 0, v_j = 0, calculate energy_00
+            ## 2. set v_i = 0, v_j = 1, calculate energy_01
+            ## 3. set v_i = 1, v_j = 0, calculate energy_10
+            ## 4. set v_i = 1, v_j = 1, calculate energy_11
+            
+            tmp_samples_v[:, i] = 0
+            tmp_samples_h[:, j] = 0
+            energy_00 = - (torch.sum(torch.matmul(tmp_samples_v, W)*tmp_samples_h, -1) + 
+                           torch.matmul(tmp_samples_v, b_v) +
+                           torch.matmul(tmp_samples_h, b_h))
+
+            tmp_samples_v[:, i] = 0
+            tmp_samples_h[:, j] = 1
+            energy_01 = - (torch.sum(torch.matmul(tmp_samples_v, W)*tmp_samples_h, -1) + 
+                           torch.matmul(tmp_samples_v, b_v) +
+                           torch.matmul(tmp_samples_h, b_h))
+
+            tmp_samples_v[:, i] = 1
+            tmp_samples_h[:, j] = 0
+            energy_10 = - (torch.sum(torch.matmul(tmp_samples_v, W)*tmp_samples_h, -1) + 
+                           torch.matmul(tmp_samples_v, b_v) +
+                           torch.matmul(tmp_samples_h, b_h))
+
+            tmp_samples_v[:, i] = 1
+            tmp_samples_h[:, j] = 1
+            energy_11 = - (torch.sum(torch.matmul(tmp_samples_v, W)*tmp_samples_h, -1) + 
+                           torch.matmul(tmp_samples_v, b_v) +
+                           torch.matmul(tmp_samples_h, b_h))
+
+            ## combined energy_00, energy_01, energy_10, and energy_11 into
+            ## an energy matrix used by MBAR
+            energy = torch.stack((energy_00, energy_01, energy_10, energy_11))
+
+            ## count number of samples for eacho of the four different states
+            ## 1. (v_i, h_j) = (0, 0)
+            ## 2. (v_i, h_j) = (0, 1)
+            ## 3. (v_i, h_j) = (1, 0)
+            ## 4. (v_i, h_j) = (1, 1)            
+            samples = torch.stack((samples_v[:,i], samples_h[:,j])).t()
+            samples = samples.tolist()
+            count = (samples.count([0,0]), samples.count([0,1]),
+                     samples.count([1,0]), samples.count([1,1]))
+
+            ## calculate free energies (negative log of partitioin function) of
+            ## the four states using MBAR
+            mbar = FastMBAR(energy.numpy(), np.array(count), cuda = False)
+            F, _ = mbar.calculate_free_energies(bootstrap = False)
+            F = torch.from_numpy(F)
+            
+            ## convert free energyies into probability
+            Z = torch.exp(-F)
+            prob_mbar[i,j] = Z[-1] / torch.sum(Z)
+            
+    return prob_mbar
+
+def calculate_angle(v1, v2):
+    v1_norm = torch.sqrt(torch.sum(v1**2))
+    v2_norm = torch.sqrt(torch.sum(v2**2))    
+    innner_product = torch.sum(v1 * v2)
+    angle = torch.acos(innner_product / (v1_norm * v2_norm))
+    return angle / math.pi * 180
--- main.py
+++ main.py
@@ -7,19 +7,21 @@ import torch.nn.functional as F
 
 import itertools
 import sys
-
 from function import *
 
-num_visible_units = 10
+sys.path.append("/home/xqding/course/projectsOnGitHub/FastMBAR/FastMBAR")
+from FastMBAR import *
+
+num_visible_units = 60
 num_hidden_units = 10
 
-torch.random.manual_seed(0)
+#torch.random.manual_seed(0)
 W = torch.randn((num_visible_units,
                  num_hidden_units))
 b_v = torch.randn(num_visible_units)
 b_h = torch.randn(num_hidden_units)
 
-prob = calculate_model_expection(W, b_v, b_h)
+prob = calculate_model_expectation_brute_force(W, b_v, b_h)
 
 v = torch.randint(0, 2, (num_visible_units, ))
 burn_in_steps = 10000
@@ -34,7 +36,7 @@ for i in range(burn_in_steps):
     random_u = torch.rand(num_visible_units)
     v = (random_u <= prob_v).float()
         
-num_steps = 5
+num_steps = 20
 samples_v = []
 samples_h = []
 for i in range(num_steps):
@@ -48,11 +50,70 @@ for i in range(num_steps):
     random_u = torch.rand(num_visible_units)
     v = (random_u <= prob_v).float()
 
-    samples_h.append(h.int())
-    samples_v.append(v.int())
+    if i % 2 == 0:
+        samples_h.append(h.int())
+        samples_v.append(v.int())
 
 samples_h = torch.stack(samples_h)
 samples_v = torch.stack(samples_v)
-prob_hat = torch.matmul(samples_v.t(), samples_h).float() / num_steps
+prob_sample = torch.matmul(samples_v.t(), samples_h).float() / samples_h.shape[0]
+
+prob_cd_1 = torch.matmul(prob_v.view((-1,1)), h.view((1,-1)))
+prob_mbar = calculate_model_expectation_mbar(W, b_v, b_h, samples_v, samples_h)
+
+diff_sample = torch.mean(torch.abs(prob_sample - prob)).item()
+diff_cd_1 = torch.mean(torch.abs(prob_cd_1 - prob)).item()
+diff_mbar = torch.mean(torch.abs(prob_mbar - prob)).item()
+
+angle_sample = calculate_angle(prob_sample.view(-1), prob.view(-1))
+angle_cd_1 = calculate_angle(prob_cd_1.view(-1), prob.view(-1))
+angle_mbar = calculate_angle(prob_mbar.view(-1), prob.view(-1))
+
+print("diff_sample: {:.3f}, diff_cd_1: {:.3f}, diff_mbar: {:.3f}".format(diff_sample, diff_cd_1, diff_mbar))
+print("angle_sample: {:.3f}, angle_cd_1: {:.3f}, angle_mbar: {:.3f}".format(angle_sample, angle_cd_1, angle_mbar))
+
+
+sys.exit()
+
+i = 5
+j = 4
+
+tmp_samples_v = samples_v.clone().float()
+tmp_samples_h = samples_h.clone().float()
+
+tmp_samples_v[:, i] = 0
+tmp_samples_h[:, j] = 0
+energy_00 = - (torch.sum(torch.matmul(tmp_samples_v, W)*tmp_samples_h, -1) + 
+               torch.matmul(tmp_samples_v, b_v) +
+               torch.matmul(tmp_samples_h, b_h))
+
+tmp_samples_v[:, i] = 0
+tmp_samples_h[:, j] = 1
+energy_01 = - (torch.sum(torch.matmul(tmp_samples_v, W)*tmp_samples_h, -1) + 
+               torch.matmul(tmp_samples_v, b_v) +
+               torch.matmul(tmp_samples_h, b_h))
+
+tmp_samples_v[:, i] = 1
+tmp_samples_h[:, j] = 0
+energy_10 = - (torch.sum(torch.matmul(tmp_samples_v, W)*tmp_samples_h, -1) + 
+               torch.matmul(tmp_samples_v, b_v) +
+               torch.matmul(tmp_samples_h, b_h))
+
+tmp_samples_v[:, i] = 1
+tmp_samples_h[:, j] = 1
+energy_11 = - (torch.sum(torch.matmul(tmp_samples_v, W)*tmp_samples_h, -1) + 
+               torch.matmul(tmp_samples_v, b_v) +
+               torch.matmul(tmp_samples_h, b_h))
+
+samples = torch.stack((samples_v[:,i], samples_h[:,j])).t()
+samples = samples.tolist()
+count = (samples.count([0,0]), samples.count([0,1]),
+         samples.count([1,0]), samples.count([1,1]))
+
+energy = torch.stack((energy_00, energy_01, energy_10, energy_11))
+mbar = FastMBAR(energy.numpy(), np.array(count), cuda = False)
+F, _ = mbar.calculate_free_energies(bootstrap = False)
+Z = np.exp(-F)
+prob_mbar_ij = Z[-1] / np.sum(Z)
 
 sys.exit()
